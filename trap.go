6d49807805a8cf0a97a539f3b869115119ee6580
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-06 13:30:09 -0700

    Space

--- trap.go
+++ trap.go
@@ -10,8 +10,8 @@ func TrapStart(stop chan<- bool) {
 	Log("trap.start")
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	Log("trap.listening")
 	sig := <-sigs
+
 	Log("trap.caught signal=%s", sig.String())
 	stop <- true
 }
