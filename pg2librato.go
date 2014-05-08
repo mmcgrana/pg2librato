a2d9bd6d790948a0175532e25d3af4e2413ae2fa
Mark McGranaghan <mmcgrana@gmail.com>
2014-05-07 23:08:31 -0700

    Move in scheduler and monitor

--- pg2librato.go
+++ pg2librato.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"time"
+)
+
 func main() {
 	Log("main.start")
 
@@ -17,4 +21,23 @@ func main() {
 	go SchedulerStart(queryFiles, queryInterval, queryTicks)
 
 	<-make(chan bool)
+}
+
+func MonitorStart(queries chan QueryFile, metrics chan []interface{}, interval int) {
+	Log("monitor.start")
+	for {
+		Log("monitor.tick queries=%d metrics=%d", len(queries), len(metrics))
+		<-time.After(time.Duration(interval) * time.Second)
+	}
+}
+
+func SchedulerStart(queryFiles []QueryFile, queryInterval int, queryTicks chan<- QueryFile) {
+	Log("scheduler.start")
+	for {
+		Log("scheduler.tick")
+		for _, queryFile := range queryFiles {
+			queryTicks <- queryFile
+		}
+		<-time.After(time.Duration(queryInterval) * time.Second)
+	}
 }
